efc3fe3019a7aacd0cb03a5429d12637d6fb8e5c
Yuriel849 <yuriel849sp@gmail.com>
2019-03-11 18:22:15 +0900

    CRU for CartService

--- CloudKitchen_template/Kitchen_Rest/src/main/java/org/badgers/rest/customer/cart/persistence/CartMapper.java
+++ CloudKitchen_template/Kitchen_Rest/src/main/java/org/badgers/rest/customer/cart/persistence/CartMapper.java
@@ -1,15 +1,24 @@
 package org.badgers.rest.customer.cart.persistence;
 
+import java.util.List;
+
 import org.badgers.rest.model.CartDetailVo;
 import org.badgers.rest.model.CartVoExtend;
 
 public interface CartMapper {
 
-	// CartServiceImpl의 addCart에서 사용
+	// CartServiceImpl의 addCart & addOptions에서 사용
 	public int getCartIdx() throws Exception;
-	public void insertCart(CartVoExtend cart) throws Exception;
+	public int insertCart(CartVoExtend cart) throws Exception;
 	public int getDetIdx() throws Exception;
-	public void insertDetails(CartDetailVo option) throws Exception;
-	 
+	public int insertDetails(CartDetailVo option) throws Exception;
+	
+	// CartServiceImpl의 readCart & readOptions에서 사용
+	public List<CartVoExtend> readCart(String custId) throws Exception;
+	public List<CartDetailVo> readOptions(String cartId) throws Exception;
+	
+	// CartServiceImpl의 updateCart & updateOptions에서 사용
+	public int updateCart(CartVoExtend cart) throws Exception;
+	public int updateOptions(CartDetailVo option) throws Exception;
 	
 }
--- CloudKitchen_template/Kitchen_Rest/src/main/java/org/badgers/rest/customer/cart/service/CartServiceImpl.java
+++ CloudKitchen_template/Kitchen_Rest/src/main/java/org/badgers/rest/customer/cart/service/CartServiceImpl.java
@@ -17,7 +17,7 @@ public class CartServiceImpl implements CartService {
 	@Setter(onMethod_ = @Autowired)
 	private CartMapper mapper;
 
-	// C : 장바구니 추가 (한 번에 메뉴 하나 BUT 수량은 1개 이상 복수 추가 가능)
+	// 메뉴 추가 (한 번에 메뉴 하나 BUT 수량은 1개 이상 복수 추가 가능)
 	@Transactional
 	@Override
 	public int addCart(CartVoExtend cart) {
@@ -30,46 +30,119 @@ public class CartServiceImpl implements CartService {
 			cart.setId(newCartId);
 			
 			// cart 정보로 cart 테이블에 추가
-			mapper.insertCart(cart);
+			returnVal = mapper.insertCart(cart);
+//			System.out.println("RETURNVAL IS : " + returnVal);
 			
+			returnVal = addOptions(cart.getOptions(), cart.getId());
+//			System.out.println("RETURNVAL IS : " + returnVal);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return returnVal;
+	}
+
+	// 옵션 추가
+	@Transactional
+	@Override
+	public int addOptions(List<CartDetailVo> options, String cartId) {
+		int returnVal = 0;
+		
+		try {
 			// cart에 포함된 List<CartDetailVo>로 cart_detail 테이블에 추가
-			List<CartDetailVo> options = cart.getOptions();
 			for(CartDetailVo option : options) {
 				// cart_detail 테이블에서 idx 값 가져와서 id 값 생성하기
 				int currentDetIdx = mapper.getDetIdx();
 				String newDetId = "cart_detail_" + currentDetIdx;
 				option.setId(newDetId);
-				option.setCartId(newCartId);
-				System.out.println(option);
-				mapper.insertDetails(option);
-			}
-			
-			// 위 작업 모두 성공적으로 수행되었을 경우 1 반환
-			returnVal = 1;
+				option.setCartId(cartId);
+				returnVal = mapper.insertDetails(option);
+//			System.out.println("RETURNVAL IS : " + returnVal);
+			}			
 		} catch (Exception e) {
 			e.printStackTrace();
-		}		
+		}
+		
 		return returnVal;
 	}
 
-	// R : 장바구니 읽기 (페이지 로딩할 때 & 결제로 넘어갈 때)
+	// 메뉴 읽기 (페이지 로딩할 때 & 결제로 넘어갈 때)
+	@Transactional
 	@Override
 	public List<CartVoExtend> readCart(String custId) {
-//		return mapper.read(custId);
-		return null;
+		List<CartVoExtend> results = null;
+		
+		try {
+			results = mapper.readCart(custId);
+			for(CartVoExtend menu : results) {
+				menu.setOptions(readOptions(menu.getId()));
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return results;
 	}
 
-	// U : 장바구니 업데이트 (메뉴 수량과 별도 버튼으로 옵션 코드 & 수량 수정 가능 -> 수량을 0으로 만들 수는 없음)
+	// 옵션 읽기
+	@Override
+	public List<CartDetailVo> readOptions(String cartId) {
+		List<CartDetailVo> result = null;
+		
+		try {
+			result = mapper.readOptions(cartId);			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return result;
+	}
+
+	// 메뉴 업데이트 (수량을 0으로 만들 수는 없음)
 	@Override
 	public int updateCart(CartVoExtend cart) {
-//		return mapper.update(cart);
+		int returnVal = 0;
+		
+		try {
+			// cart 정보로 cart 테이블 수정
+			returnVal = mapper.updateCart(cart);
+//			System.out.println("RETURNVAL IS : " + returnVal);
+			
+			returnVal = updateOptions(cart.getOptions());
+//			System.out.println("RETURNVAL IS : " + returnVal);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return returnVal;
+	}
+
+	// 옵션 업데이트
+	@Override
+	public int updateOptions(List<CartDetailVo> options) {
+		int returnVal = 0;
+		
+		try {
+			for(CartDetailVo option : options) {
+				returnVal = mapper.updateOptions(option);
+			}			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		return returnVal;
+	}
+
+	// 메뉴 삭제 (하나씩 개별 삭제 OR 별도 버튼으로 전체 삭제)
+	@Override
+	public int deleteCart(List<String> custIdList) {
 		return 0;
 	}
 
-	// D : 장바구니에서 삭제 (하나씩 개별 삭제 OR 별도 버튼으로 전체 삭제)
+	// 옵션 삭제
 	@Override
-	public int deleteCart(String custId) {
-//		return mapper.delete(custId);
+	public int deleteOptions(String cartId) {
 		return 0;
 	}
+
 }
