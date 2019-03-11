1a7785b4aa2aee8685617de2b47b709d8c61d556
Yuriel849 <yuriel849sp@gmail.com>
2019-03-11 12:26:16 +0900

    temp save of work on cart

--- CloudKitchen_template/Kitchen_Rest/src/main/java/org/badgers/rest/customer/cart/service/CartService.java
+++ CloudKitchen_template/Kitchen_Rest/src/main/java/org/badgers/rest/customer/cart/service/CartService.java
@@ -1,13 +1,20 @@
 package org.badgers.rest.customer.cart.service;
 
+import java.util.List;
+
+import org.badgers.rest.model.CartVoExtend;
+
 public interface CartService {
 
-	// 장바구니 추가
+	// C : 장바구니 추가 (한 번에 메뉴 하나 BUT 수량은 1개 이상 복수 추가 가능)
+	public int addCart(CartVoExtend cart);
+
+	// R : 장바구니 읽기 (페이지 로딩할 때 & 결제로 넘어갈 때)
+	public List<CartVoExtend> readCart(String custId);
 	
-	// 장바구니 업데이트
+	// U : 장바구니 업데이트 (메뉴 수량과 별도 버튼으로 옵션 코드 & 수량 수정 가능 -> 수량을 0으로 만들 수는 없음)
+	public int updateCart(CartVoExtend cart);	
 	
-	// 장바구니 한 개 삭제
-	
-	// 장바구니에서 모두 삭제 (결제하기로 넘어갈 때)
-	
+	// D : 장바구니에서 삭제 (하나씩 개별 삭제 OR 별도 버튼으로 전체 삭제)
+	public int deleteCart(String custId);
 }
