cf7030ade7617ebb6d25a497faa83b4ffe6a1d9c
EuniceSon <sonyoon7@gmail.com>
2019-03-12 15:24:37 +0900

    commit

--- CloudKitchen_template/Kitchen_Rest/src/main/java/org/badgers/rest/customer/order/controller/CustOrderController.java
+++ CloudKitchen_template/Kitchen_Rest/src/main/java/org/badgers/rest/customer/order/controller/CustOrderController.java
@@ -1,14 +1,20 @@
 package org.badgers.rest.customer.order.controller;
 
+import java.util.List;
+
 import org.badgers.rest.customer.order.service.CustOrderService;
+import org.badgers.rest.model.OrderInfoVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.Setter;
 
-@Controller
+@RestController
 public class CustOrderController {
 	
 	@Setter(onMethod_ = { @Autowired })
@@ -20,6 +26,18 @@ public class CustOrderController {
 		service.excuteOrder();
 		return "home";
 	}
+	
+	@GetMapping("/order/{orderId}")
+	public List<OrderInfoVO> getOrderInfo(
+			@PathVariable("orderId") String orderId
+			) throws Exception{
+		System.out.println(orderId);
+		service.getOrderInfo();
+				return null;
+		
+	}
+	
+	
 	
 	
 }
