a379985960155c3ae02fd420a1415785a596ee97
Yuriel849 <yuriel849sp@gmail.com>
2019-03-12 14:40:54 +0900

    temp save (to pull and merge develop)

--- CloudKitchen_template/Kitchen_Rest/src/main/java/org/badgers/rest/customer/cart/controller/CartController.java
+++ CloudKitchen_template/Kitchen_Rest/src/main/java/org/badgers/rest/customer/cart/controller/CartController.java
@@ -2,13 +2,21 @@ package org.badgers.rest.customer.cart.controller;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import org.badgers.rest.customer.cart.service.CartService;
+import org.badgers.rest.model.CartVoExtend;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,22 +32,27 @@ public class CartController {
 
 	private static final Logger logger = LoggerFactory.getLogger(CartController.class);
 	
-	/**
-	 * Simply selects the home view to render by returning its name.
-	 */
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String home(Locale locale, Model model) {
-		logger.info("Welcome home! The client locale is {}.", locale);
-		
-		Date date = new Date();
-		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
-		
-		String formattedDate = dateFormat.format(date);
-		
-		model.addAttribute("serverTime", formattedDate );
-		
-		logger.info("The current server time is " + formattedDate);
-		
-		return "home";
+	@PostMapping("/")
+	public int addCart(@RequestBody CartVoExtend cart) {
+		logger.info("Adding " + cart.getCustId() + "'s new item to the 'cart' & 'cart_detail' tables!");
+		return 0;
+	}
+	
+	@GetMapping("/{custId}")
+	public List<CartVoExtend> readCart(@PathVariable("custId") String custId) {
+		logger.info("Reading " + custId + "'s items from the 'cart' table!");
+		return null;
+	}
+	
+	@PutMapping("/")
+	public int updateCart(@RequestBody CartVoExtend cart) {
+		logger.info("Updating " + cart.getCustId() + "'s items in the 'cart' table!");
+		return 0;
+	}
+	
+	@DeleteMapping("/{custId}/{cartId}")
+	public int deleteCart(@PathVariable("custId") String custId, @PathVariable("cartId") String cartId) {
+		logger.info("Deleting " + custId + "'s item(s) from the 'cart' & 'cart_detail' tables!");
+		return 0;
 	}
 }
