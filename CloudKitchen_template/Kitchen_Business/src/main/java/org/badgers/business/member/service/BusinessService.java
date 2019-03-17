d9fcc2d7d0fd4a8faa6d693438492cf3379548b4
Yuriel849 <yuriel849sp@gmail.com>
2019-03-17 22:52:16 +0900

    RestTemplate 테스트

--- CloudKitchen_template/Kitchen_Business/src/main/java/org/badgers/business/member/service/BusinessService.java
+++ CloudKitchen_template/Kitchen_Business/src/main/java/org/badgers/business/member/service/BusinessService.java
@@ -1,5 +1,9 @@
 package org.badgers.business.member.service;
 
+import org.badgers.business.model.BizMemberVo;
+import org.springframework.http.ResponseEntity;
+
 public interface BusinessService {
 
+	public ResponseEntity<BizMemberVo> readBizMember(String bizId) throws Exception;
 }
--- CloudKitchen_template/Kitchen_Business/src/main/java/org/badgers/business/member/service/BusinessServiceImpl.java
+++ CloudKitchen_template/Kitchen_Business/src/main/java/org/badgers/business/member/service/BusinessServiceImpl.java
@@ -1,8 +1,47 @@
 package org.badgers.business.member.service;
 
+import java.util.List;
+
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 @Service
-public class BusinessServiceImpl implements BusinessService {
+public class BusinessServiceImpl {
+	public static void main(String[] args) {
+		
+		System.out.println("started.........");
+		
+		HttpComponentsClientHttpRequestFactory factory = new HttpComponentsClientHttpRequestFactory();
 
+		// HttpClient객체는 커넥션풀을 기본으로 쓰지 않기 때문에 Route와 연결수를 지정해서 DBCP처럼 유지시켜줘야함
+		HttpClient httpClient = HttpClientBuilder.create().setMaxConnPerRoute(5).build();
+		factory.setHttpClient(httpClient);
+		
+		//해당 정보를 바탕으로 restTemplate을 만듬
+		RestTemplate restTemplate = new RestTemplate(factory);
+		System.out.println("restTemplate is : " + restTemplate);
+		
+		try {
+			String url = "http://localhost/business/biz_1/mypage";
+			System.out.println("here I am");
+			ResponseEntity<List> responseEntity = restTemplate.getForEntity(url, List.class);
+			System.out.println("here I am");
+			
+			List<?> bizMember = responseEntity.getBody();
+
+			System.out.println(responseEntity);
+			System.out.println(bizMember);
+
+			for(int i = 0; i < bizMember.size(); i++) {
+				System.out.println(bizMember.get(i));
+			}
+
+		}catch(Exception e){
+			e.getStackTrace();
+		}
+	}
 }
