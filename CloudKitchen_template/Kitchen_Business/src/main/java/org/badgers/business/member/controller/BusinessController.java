191705260c02a0daabc0ed69beccdfe73905c0fe
Yuriel849 <yuriel849sp@gmail.com>
2019-03-18 12:29:07 +0900

    controller 수정

--- CloudKitchen_template/Kitchen_Business/src/main/java/org/badgers/business/member/controller/BusinessController.java
+++ CloudKitchen_template/Kitchen_Business/src/main/java/org/badgers/business/member/controller/BusinessController.java
@@ -1,29 +1,42 @@
 package org.badgers.business.member.controller;
 
 import org.badgers.business.member.service.BusinessService;
+import org.badgers.business.model.BizMemberVOExtend;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import lombok.Setter;
+import lombok.extern.log4j.Log4j;
 
 @Controller
 @RequestMapping("/member")
+@Log4j
 public class BusinessController {
 
 	@Setter(onMethod_ = { @Autowired })
 	private BusinessService service;
 	
 	@GetMapping("/{bizId}")
-	public void readBizMember(@PathVariable("bizId") String bizId) {
+	public ModelAndView readBizMember(@PathVariable("bizId") String bizId) {
+		ModelAndView mav = new ModelAndView();
+		
+		BizMemberVOExtend returnVal = null;
+		
 		try {
-			service.readBizMember(bizId);
+			returnVal = service.readBizMember(bizId);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		
-		System.out.println("done");
+		log.info("readBizMember DONE!!!!!");
+		
+		mav.addObject("bizMember", returnVal);
+		mav.setViewName("mypage");
+		
+		return mav;
 	}
 }
