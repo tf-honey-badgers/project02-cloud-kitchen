ae5f043bd5561c875cac23ce1c2ee653fa6f8c86
JOON <shot013@naver.com>
2019-03-18 20:12:34 +0900

    20190318_20:12

--- CloudKitchen_template/Kitchen_Business/src/main/java/org/badgers/business/menu/controller/MenuController.java
+++ CloudKitchen_template/Kitchen_Business/src/main/java/org/badgers/business/menu/controller/MenuController.java
@@ -1,14 +1,11 @@
 package org.badgers.business.menu.controller;
 
-import java.util.List;
-
 import org.badgers.business.menu.service.MenuServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import lombok.Setter;
@@ -20,12 +17,12 @@ public class MenuController {
 	@Setter(onMethod_ = { @Autowired })
 	private MenuServiceImpl service;
 	
-	@RequestMapping(value="/{bizId}", method=RequestMethod.GET)
-	@ResponseBody
-	public List<?> bizGetMenu(ModelAndView mav, @PathVariable("bizId") String bizId) {
-		System.out.println(bizId);
-//		mav.addObject("bizMenu", service.bizGetMenu(bizId));
-		return service.bizGetMenu(bizId);
+	@RequestMapping(value="/update/{bizId}", method=RequestMethod.GET)
+	public ModelAndView bizGetMenu(ModelAndView mav, @PathVariable("bizId") String bizId) {
+		mav.addObject("bizMenu", service.bizGetMenu(bizId));
+		mav.setViewName("menuupdate");
+		
+		return mav;
 	}
 	
 	
