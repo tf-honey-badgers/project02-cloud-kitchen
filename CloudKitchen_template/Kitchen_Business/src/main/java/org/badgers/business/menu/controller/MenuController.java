8df7e463071c407b8e8d914af376398f1a7e3b05
JOON <shot013@naver.com>
2019-03-18 12:13:13 +0900

    20190318_12:12 controller -> restController 요청 테스트 성공

--- CloudKitchen_template/Kitchen_Business/src/main/java/org/badgers/business/menu/controller/MenuController.java
+++ CloudKitchen_template/Kitchen_Business/src/main/java/org/badgers/business/menu/controller/MenuController.java
@@ -1,14 +1,26 @@
 package org.badgers.business.menu.controller;
 
-import org.badgers.business.menu.service.MenuService;
+import java.util.List;
+
+import org.badgers.business.menu.service.MenuServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import lombok.Setter;
 
 @Controller
+@RequestMapping("/menu")
 public class MenuController {
 	
 	@Setter(onMethod_ = { @Autowired })
-	private MenuService service;
+	private MenuServiceImpl service;
+	
+	@RequestMapping("/{bizId}")
+	@ResponseBody
+	public List<?> bizGetMenu(@PathVariable("bizId") String bizId){
+		return service.bizGetMenu(bizId);
+	}
 }
