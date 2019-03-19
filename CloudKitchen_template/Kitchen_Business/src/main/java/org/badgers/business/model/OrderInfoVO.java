6e77a834456257092b2365bb688070d72b78da4f
EuniceSon <sonyoon7@gmail.com>
2019-03-19 16:44:57 +0900

    restTemplate 논란

---
+++ CloudKitchen_template/Kitchen_Business/src/main/java/org/badgers/business/model/OrderInfoVO.java
@@ -0,0 +1,29 @@
+package org.badgers.business.model;
+
+import lombok.Data;
+
+@Data
+public class OrderInfoVO {
+	private String id;
+	private String custId;
+	private String orderdetailId;
+	private String phone;
+	private String address;
+	private String time;
+	private String statusCode;
+	private String status;
+	private String kitchenName;
+	private String bizId;
+	private String bizName; 
+	private String method; 
+	private String menuName;
+	private int menuPrice;
+	private String optName;
+	private int optPrice;
+	private int quantity;
+	private int totalAmt;
+	private int payAmt;
+	private String msg;
+	private String requestMsg;
+
+}
