ad7a592981d69fa83a4900b78e64131bb65afbcf
Yuriel849 <yuriel849sp@gmail.com>
2019-03-23 16:15:33 +0900

    DB에서 menu_id를 int로 수정한 것을 VO에 반영

--- CloudKitchen_template/Kitchen_Rest/src/main/java/org/badgers/rest/model/CartDetailVO.java
+++ CloudKitchen_template/Kitchen_Rest/src/main/java/org/badgers/rest/model/CartDetailVO.java
@@ -11,11 +11,11 @@ public class CartDetailVO {
 	private int id;
 	private int cartId; // FK -> cart 테이블 id 속성 참조
 	private int menuOptId; // FK -> menu_option 테이블 id 속성 참조
-	private String menuId;
+	private int menuId; // CartVOExtend의 menuId와 동일
 	private String menuOptName;
 	private int menuOptPrice;
 	
-	public CartDetailVO(int optId, String menuId, String optName, int optPrice) {
+	public CartDetailVO(int optId, int menuId, String optName, int optPrice) {
 		this.menuOptId = optId;
 		this.menuId = menuId;
 		this.menuOptName = optName;
