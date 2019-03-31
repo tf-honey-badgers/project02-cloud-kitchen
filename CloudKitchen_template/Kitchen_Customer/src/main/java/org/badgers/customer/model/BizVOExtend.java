f74cb876d6be0540ea3d8fd1903dca5f9edbb707
Yuriel849 <yuriel849sp@gmail.com>
2019-03-31 19:01:50 +0900

    가게 종류명 변수 추가, CommonCodeVO 추가

--- CloudKitchen_template/Kitchen_Customer/src/main/java/org/badgers/customer/model/BizVOExtend.java
+++ CloudKitchen_template/Kitchen_Customer/src/main/java/org/badgers/customer/model/BizVOExtend.java
@@ -11,7 +11,8 @@ public class BizVOExtend {
 	private String bizName;  // 가게 상호명 
 	private int bizLikeCnt; //찜횟수 
 	private int bizMinAmt;  // 최소 주문 금액
-	private String bizCatId; // 가게  메뉴 카테고리 ? 
+	private String bizCatId; // 가게  종류 카테고리 (한식 등)
+	private String bizCatName; // 가게  종류명 (한식 등)
 	private String bizKitchenId; // 키친 아이디 (키친 위치 종각점인지 어딘지) 
 	private String bizLiveStrm; //라이브 시스템
 	private List<MenuCatVOExtend> bizMenuCatVo;
