ceee59b99a53065bf37f57ad13a5bc077e18b016
Yuriel849 <yuriel849sp@gmail.com>
2019-03-31 20:55:52 +0900

    VO에 키친 이름 변수 추가

--- CloudKitchen_template/Kitchen_Customer/src/main/java/org/badgers/customer/model/BizVOExtend.java
+++ CloudKitchen_template/Kitchen_Customer/src/main/java/org/badgers/customer/model/BizVOExtend.java
@@ -13,7 +13,8 @@ public class BizVOExtend {
 	private int bizMinAmt;  // 최소 주문 금액
 	private String bizCatId; // 가게  종류 카테고리 (한식 등)
 	private String bizCatName; // 가게  종류명 (한식 등)
-	private String bizKitchenId; // 키친 아이디 (키친 위치 종각점인지 어딘지) 
+	private String bizKitchenId; // 키친 아이디 (키친 위치 종각점인지 어딘지)
+	private String bizKitchenName; // 키친 이름 (키친 위치 종각점인지 어딘지)
 	private String bizLiveStrm; //라이브 시스템
 	private List<MenuCatVOExtend> bizMenuCatVo;
 	private int bizIdx;
