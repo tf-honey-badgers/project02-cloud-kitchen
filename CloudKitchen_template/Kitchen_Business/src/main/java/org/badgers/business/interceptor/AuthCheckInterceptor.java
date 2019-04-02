629678092ea7c0e42b064d1fb71cadcc9cca2297
EuniceSon <sonyoon7@gmail.com>
2019-04-02 12:56:23 +0900

    비지니스 사업자 보안 수정

--- CloudKitchen_template/Kitchen_Business/src/main/java/org/badgers/business/interceptor/AuthCheckInterceptor.java
+++ CloudKitchen_template/Kitchen_Business/src/main/java/org/badgers/business/interceptor/AuthCheckInterceptor.java
@@ -1,9 +1,12 @@
 package org.badgers.business.interceptor;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.springframework.web.servlet.HandlerMapping;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import lombok.extern.log4j.Log4j;
@@ -13,15 +16,34 @@ public class AuthCheckInterceptor extends HandlerInterceptorAdapter{
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		final Map<String, String> pathVariables = (Map<String, String>) request
+                    .getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		//pathvariable 에서 가져온 bizId 값 
+		final String uriBizId=pathVariables.get("bizId");
+		log.info("uriBizId   : "+uriBizId);
+		
 		
 		HttpSession session = request.getSession();
 		log.info(session.getAttribute("bizId"));
+		//세션에 저장되어 있는 bizId 값
+		String sessionBizId = (String) session.getAttribute("bizId");
+		
+		
+		//세션자체가 null 이면 false 반환
+		
 		if(session.getAttribute("bizId")==null) {
 			log.info("--------------------세션 체크 null-------------------------");
 			response.sendRedirect("/business/main?msg=fail");
             return false;
 			
 		}
+		
+		//세션은 있는데 세션id랑 pathvariable id랑 비교해서 일치하는지 보여줘야함 
+		if(sessionBizId.equals(uriBizId)==false) {
+			response.sendRedirect("/business/main?msg=invalidId");
+			return false;
+		}
+			
 		
 		return true;
 	}
