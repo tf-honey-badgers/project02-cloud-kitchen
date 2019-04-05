26c9cc2cff045b776e07c0b47c893524c3d53d40
EuniceSon <sonyoon7@gmail.com>
2019-04-05 21:50:39 +0900

    여보세요

--- CloudKitchen_template/Kitchen_Business/src/main/java/org/badgers/business/order/controller/OrderController.java
+++ CloudKitchen_template/Kitchen_Business/src/main/java/org/badgers/business/order/controller/OrderController.java
@@ -12,13 +12,16 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
+import lombok.extern.log4j.Log4j;
+
 @RequestMapping("/order")
 @Controller
+@Log4j
 public class OrderController {
 	
 	@Inject
 	RestTemplate restTemplate;
-	
+	//사업자 주문관리 페이지로 이동
 	@GetMapping("/main")
 	public String test(OrderVO vo) {
 
@@ -26,16 +29,15 @@ public class OrderController {
 		return "/order/order";
 	}
 	
+	//주문 상태코드 변경  
 	@PutMapping("/{bizId}/{orderId}/{status}")
 	public ResponseEntity<Object> updateStatus(
 			@PathVariable("bizId") String bizId, 
 			@PathVariable("orderId") String orderId, 
 			@PathVariable("status") String status) {
-		
+		log.info("...................주문상태코드 변경  -> "+status);
 		String url = "http://localhost/rest/biz/order/"+bizId+"/"+orderId+"/"+status; 
 		Object obj = restTemplate.getForEntity(url, String.class);
-		System.out.println("=============================================================");
-		System.out.println(obj);
 		return new ResponseEntity<>(obj, HttpStatus.OK);	
 	}
 
