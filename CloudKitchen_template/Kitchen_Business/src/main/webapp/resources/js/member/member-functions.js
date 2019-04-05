1e87c76789131e9580fa8e810518bf0280df4993
JOON <shot013@naver.com>
2019-04-05 13:15:24 +0900

    20190405_business 배포용 url변경

--- CloudKitchen_template/Kitchen_Business/src/main/webapp/resources/js/member/member-functions.js
+++ CloudKitchen_template/Kitchen_Business/src/main/webapp/resources/js/member/member-functions.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 	/* 사용자 정보 중 계좌정보 수정하기 */
 	$('#changeAccount').on('click', function() {
 		$.ajax({
-    		url : 'http://localhost:3000/business/member/' + $('#bizId').val() + '/modify'
+    		url : 'http://13.209.21.25/business/member/' + $('#bizId').val() + '/modify'
     		, type : 'POST'
 			, contentType : 'application/json'
     		, data : JSON.stringify({bizId : $('#bizId').val(), account : $('#account').val()})
@@ -15,7 +15,7 @@ $(document).ready(function() {
 	/* 가게 정보 중 최소주문금액, 생방송 키 코드, 가게 소개글 수정하기 */
 	$('#changeBiz').on('click', function() {
 		$.ajax({
-    		url : 'http://localhost:3000/business/member/' + $('#bizId').val() + '/modify'
+    		url : 'http://13.209.21.25/business/member/' + $('#bizId').val() + '/modify'
     		, type : 'POST'
 			, contentType : 'application/json'
     		, data : JSON.stringify({
@@ -32,7 +32,7 @@ $(document).ready(function() {
 	/* YouTube 생방송 시작하면 iframe player src 속성용 코드 입력하기 (biz테이블 bizLiveStrm컬럼 사용) */
 	$('#changeBizLiveStrm').on('click', function() {
 		$.ajax({
-    		url : 'http://localhost:3000/business/member/livestrm/' + $('#bizId').val() + '/' + $('#bizLiveStrm').val()
+    		url : 'http://13.209.21.25/business/member/livestrm/' + $('#bizId').val() + '/' + $('#bizLiveStrm').val()
     		, type : 'GET'
 			, contentType : 'application/json'
     		, error : function() { md.showNotification('bottom', 'right', 'danger', 'YouTube LiveStreaming 코드를 입력하는데 에러가 발생했습니다.'); }
@@ -70,7 +70,7 @@ $(document).ready(function() {
 		
 		if(cnt===0){
 			$.ajax({
-	    		url : 'http://localhost:3000/business/member/login'
+	    		url : 'http://13.209.21.25/business/member/login'
 	    		, type : 'POST'
 				, contentType : 'application/json'
 				, dataType : 'text'
@@ -117,7 +117,7 @@ $(document).ready(function() {
 		$('#finder').modal('show');
     	$('#getId').on('click', function() {
     		$.ajax({
-        		url : 'http://localhost:3000/business/member/verify'
+        		url : 'http://13.209.21.25/business/member/verify'
         		, type : 'POST'
 				, contentType : 'application/json'
         		, data : JSON.stringify({
@@ -157,7 +157,7 @@ $(document).ready(function() {
 		$('#finder').modal('show');
     	$('#getId').on('click', function() {
     		$.ajax({
-        		url : 'http://localhost:3000/business/member/verify'
+        		url : 'http://13.209.21.25/business/member/verify'
         		, type : 'POST'
 				, contentType : 'application/json'
         		, data : JSON.stringify({
@@ -219,7 +219,7 @@ $(document).ready(function() {
 		}
 
 		$.ajax({
-    		url : 'http://localhost:3000/business/member/' + hiddenId + '/modify'
+    		url : 'http://13.209.21.25/business/member/' + hiddenId + '/modify'
     		, type : 'POST'
 			, contentType : 'application/json'
     		, data : JSON.stringify({
