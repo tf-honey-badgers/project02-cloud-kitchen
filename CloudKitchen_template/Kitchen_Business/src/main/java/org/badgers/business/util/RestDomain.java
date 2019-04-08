4d2cc2bf2e320febda16d235dc52eddc65a06134
JOON <shot013@naver.com>
2019-04-08 16:59:33 +0900

    dd

--- CloudKitchen_template/Kitchen_Business/src/main/java/org/badgers/business/util/RestDomain.java
+++ CloudKitchen_template/Kitchen_Business/src/main/java/org/badgers/business/util/RestDomain.java
@@ -2,5 +2,5 @@ package org.badgers.business.util;
 
 public class RestDomain {
 //	public static final String restDomain ="http://13.209.21.25/rest"; 
-	public static final String restDomain ="http://localhost/rest"; 
+	public static final String restDomain ="http://localhost/rest";
 }
