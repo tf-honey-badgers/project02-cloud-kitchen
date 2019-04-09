e59fd545c3fc47b3d9d2bb4b28bcc1d16a0eb7d4
JOON <shot013@naver.com>
2019-04-09 18:09:42 +0900

    20190409_18:09 mahout 완

--- CloudKitchen_template/Kitchen_Rest/src/main/java/org/badgers/rest/customer/review/service/ReviewService.java
+++ CloudKitchen_template/Kitchen_Rest/src/main/java/org/badgers/rest/customer/review/service/ReviewService.java
@@ -4,10 +4,12 @@ import java.util.List;
 
 import org.apache.mahout.cf.taste.recommender.RecommendedItem;
 import org.badgers.rest.model.GpaVOExtend;
+import org.badgers.rest.model.MenuVOExtend;
 import org.badgers.rest.model.ReviewVO;
 
 public interface ReviewService {
 	public List<ReviewVO> revList();
 	public List<RecommendedItem> recommendation(Long custIdx);
 	public List<GpaVOExtend> getGpa(String bizId);
+	public List<MenuVOExtend> getRecommendMenu(String menuId);
 }
