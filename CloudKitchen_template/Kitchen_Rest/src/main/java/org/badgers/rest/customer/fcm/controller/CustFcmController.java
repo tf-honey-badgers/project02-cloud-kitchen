08055e492f17ed453c9510071292ff87d530d881
EuniceSon <sonyoon7@gmail.com>
2019-04-11 10:19:21 +0900

    fcm

---
+++ CloudKitchen_template/Kitchen_Rest/src/main/java/org/badgers/rest/customer/fcm/controller/CustFcmController.java
@@ -0,0 +1,13 @@
+package org.badgers.rest.customer.fcm.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+@Controller
+public class CustFcmController {
+	
+	
+	
+}
---
+++ CloudKitchen_template/Kitchen_Rest/src/main/java/org/badgers/rest/model/FcmVO.java
@@ -0,0 +1,11 @@
+package org.badgers.rest.model;
+
+import lombok.Data;
+
+@Data
+public class FcmVO {
+	
+	private String custID;
+	private String Token;
+
+}
