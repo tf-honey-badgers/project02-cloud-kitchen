db77c2593c314a7530d5e5d20baf27daf1baf1c4
EuniceSon <sonyoon7@gmail.com>
2019-04-17 21:22:22 +0900

    rest ip aws ec2 로 바꿈

--- CloudKitchen_template/Kitchen_Business/src/main/java/org/badgers/business/util/RestDomain.java
+++ CloudKitchen_template/Kitchen_Business/src/main/java/org/badgers/business/util/RestDomain.java
@@ -1,8 +1,8 @@
 package org.badgers.business.util;
 
 public class RestDomain {
-//	public static final String restDomain ="http://13.209.21.25/rest"; 
-	public static final String restDomain ="http://localhost:80/rest";
-	public static final String bizDomain ="http://localhost:3000/business";
+	public static final String restDomain ="http://54.180.106.143/rest"; 
+//	public static final String restDomain ="http://localhost:80/rest";
+//	public static final String bizDomain ="http://localhost:3000/business";
 	
 }
